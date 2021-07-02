52e22a4eab01faae5a369b16c3ff08a9b19b6e2d
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-07-01 17:01:04 -0700

    added TDD for didUserWin

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,18 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { didUserWin } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('didUserWin should return win1 if the userChoice is 1 and the computerChoice is 0 ', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const userChoice = 1;
+    const computerChoice = 0;
+    const expected = 'win1';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = didUserWin(userChoice, computerChoice);
 
     //Expect
     // Make assertions about what is expected versus the actual result
